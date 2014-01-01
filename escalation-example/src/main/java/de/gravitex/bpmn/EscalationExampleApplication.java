b0291b313af028900d728fd471cb41bb7321aab0
stefan <stefan@192.168.0.6>
2014-01-01 15:41:33 +0100

    added escalation example

---
+++ escalation-example/src/main/java/de/gravitex/bpmn/EscalationExampleApplication.java
@@ -0,0 +1,9 @@
+package de.gravitex.bpmn;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Escalation Example Application")
+public class EscalationExampleApplication extends ServletProcessApplication {
+
+}
