e045474bef4cca7617467b5f9a67f8064dfab637
stefan <stefan@192.168.0.6>
2014-01-04 22:12:21 +0100

    further work on change process

--- change-process/src/main/java/de/gravitex/bpmn/message/ChangeProcessMessages.java
+++ change-process/src/main/java/de/gravitex/bpmn/message/ChangeProcessMessages.java
@@ -5,4 +5,6 @@ public class ChangeProcessMessages {
 	public static final String MESSAGE_TRIGGER_CHANGE = "changeTriggered";
 	
 	public static final String MESSAGE_CANCEL_CHANGE = "changeCancelled";
+	
+	public static final String MESSAGE_INFORM_CUSTOMER = "informCustomer";
 }
