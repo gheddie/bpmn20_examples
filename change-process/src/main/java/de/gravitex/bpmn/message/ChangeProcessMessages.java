5326c87e272309ad51837e9ff5f6d008f34c7da4
stefan <stefan@192.168.0.6>
2014-01-04 21:30:32 +0100

    removed java maven 1.7 support (for it kills deployment on mac os)

---
+++ change-process/src/main/java/de/gravitex/bpmn/message/ChangeProcessMessages.java
@@ -0,0 +1,8 @@
+package de.gravitex.bpmn.message;
+
+public class ChangeProcessMessages {
+
+	public static final String MESSAGE_TRIGGER_CHANGE = "changeTriggered";
+	
+	public static final String MESSAGE_CANCEL_CHANGE = "changeCancelled";
+}
