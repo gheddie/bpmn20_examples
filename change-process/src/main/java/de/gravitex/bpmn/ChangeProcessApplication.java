be13d1344f2e77abdd898a0c905fd343635c201e
stefan <stefan@192.168.0.6>
2014-01-04 17:02:45 +0100

    added change process

---
+++ change-process/src/main/java/de/gravitex/bpmn/ChangeProcessApplication.java
@@ -0,0 +1,9 @@
+package de.gravitex.bpmn;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Change Process Application")
+public class ChangeProcessApplication extends ServletProcessApplication {
+
+}
