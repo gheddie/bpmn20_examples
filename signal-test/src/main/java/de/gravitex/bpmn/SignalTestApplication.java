ae322ca65360b7c061aa12b2cfed0c24ad9c1c0f
stefan.schulz <stefan.schulz@bcc-ws65.bcc.intern>
2014-01-06 13:21:07 +0100

    added signal test process

---
+++ signal-test/src/main/java/de/gravitex/bpmn/SignalTestApplication.java
@@ -0,0 +1,9 @@
+package de.gravitex.bpmn;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Signal Test Application")
+public class SignalTestApplication extends ServletProcessApplication {
+
+}
